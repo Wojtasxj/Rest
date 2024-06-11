555a21ea6c96ae82f67824f6df1351b6e915802e
Wojciech Klause <wojtasxj@gmail.com>
2024-06-11 20:43:09 +0200

    ready to publish

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const cors = require('cors');
 const testimonialsRoutes = require('./routes/testimonials.routes.js');
 const concertRoutes = require('./routes/concerts.routes.js');
 const seatsRoutes = require('./routes/seats.routes.js');
-
+const path = require('path');
 
 const app = express();
-const port = 8000;
-
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true}));
@@ -15,11 +13,18 @@ app.use(express.json());
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatsRoutes);
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+
+
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found.!.' });
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+  });
